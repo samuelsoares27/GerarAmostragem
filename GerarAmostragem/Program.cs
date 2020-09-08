919a34dff3f63b63f80470e1e051102f626f8017
Samuel Soares <34168199+samuelsoares27@users.noreply.github.com>
2020-09-08 10:26:23 -0300

    Initial commit

---
+++ GerarAmostragem/Program.cs
@@ -0,0 +1,51 @@
+﻿using IronXL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GerarAmostragem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando...");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Lendo documento...");
+
+            WorkBook workbook = WorkBook.Load("d:\\documento.xlsx");
+            WorkSheet sheet = workbook.WorkSheets.First();
+
+            var dataSet = workbook.ToDataSet();
+            var qtdLinhas = dataSet.Tables[0].Rows.Count;
+
+            var porcentagem = Math.Round(qtdLinhas * 0.10);
+
+            Random randNum = new Random();
+
+            List<int> ListaNumeroRandom = new List<int>();
+
+            for (int i = 0; i < porcentagem; i++)
+            {
+                var numero = randNum.Next(qtdLinhas);
+                if (!ListaNumeroRandom.Contains(numero))
+                {
+                    ListaNumeroRandom.Add(numero);
+                }
+                
+            }
+
+            int contador = 1;
+            foreach (var item in ListaNumeroRandom)
+            {
+                sheet[$"O{item}"].Value = "Selecionado";
+                Console.WriteLine($"{contador}/{ListaNumeroRandom.Count()}");
+                contador++;
+            }
+
+            workbook.Save();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(">>>> PRONTO!!! <<<<");
+        }
+    }
+}
